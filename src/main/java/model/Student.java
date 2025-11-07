fa4f050995c4c462737a7e505605e20e7552dddd
Maxim Dyachenko <onedayexperience808@gmail.com>
2025-11-07 23:00:57 +0700

    sec commit

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,48 @@
+package model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "email", unique = true, length = 100)
+    private String email;
+
+
+    public Student() {}
+
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
